13330e9a40c8c33f598efba019ca9d289fdd03c4
Mutisoo <emutisom@gmail.com>
2026-08-21 14:05:20 +0300

    feat(user):worked on Get User tested with PostMan and works

--- SpringJPA/JPA/src/main/java/start/spring/JPA/Models/User.java
+++ SpringJPA/JPA/src/main/java/start/spring/JPA/Models/User.java
@@ -4,21 +4,25 @@ import org.jarkarta.*;
 import org.lombok.*;
 
 @Entity
-@Table("user")
+@Table(name = "user")
 @Data
 public class User {
     @Id
-    @GeneratedValue()
-    private String Id;
-    @Column(name="username",nullable=false,unique=true)
-    private String username;
-    @Column(name="f_name",nullable=false)
-    private String fname;
-    @Column(name="s_name",nullable=false)
-    private String sname;
-    @Column(name="email",nullable=false,unique=true)
-    private String email;
-    @Column(name="email",nullable=false,unique=true)
-    private String password;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "f_name", nullable = false)
+    private String fname;
+
+    @Column(name = "s_name", nullable = false)
+    private String sname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
 }
